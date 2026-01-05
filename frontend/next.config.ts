fe7d7ed9201fcc80b236f4f7d079d3099d5b4d86
atmosinf <73055038+atmosinf@users.noreply.github.com>
2026-01-05 11:32:28 +0000

    feat: Add Dockerfiles for frontend and backend, configure Next.js for standalone output and API rewrites, and introduce a Docker Compose setup.

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL || 'http://127.0.0.1:8000'}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
